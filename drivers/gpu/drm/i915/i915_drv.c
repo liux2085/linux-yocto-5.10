a3da1983f1202fb3d058a14bd90c76b5b4eed9b6
Colin Xu <colin.xu@intel.com>
2020-10-27 12:54:06 +0800

    drm/i915: Add GVT resume routine to i915
    commit 385fc38c1f97387c47113254d8de2cb0dbf133bd upstream.
    
    This patch add gvt resume wrapper into i915_drm_resume().
    GVT relies on i915 so resume gvt at last.
    
    V2:
    - Direct call into gvt suspend/resume wrapper in intel_gvt.h/intel_gvt.c.
    The wrapper and implementation will check and call gvt routine. (zhenyu)
    
    V3:
    Refresh.
    
    V4:
    Rebase.
    
    V5:
    Fail intel_gvt_suspend() if fail to save GGTT.
    
    V6:
    Save host entry to per-vGPU gtt.ggtt_mm on each host_entry update so
    only need the resume routine.
    
    V7:
    Refresh.
    
    Reviewed-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Hang Yuan <hang.yuan@linux.intel.com>
    Signed-off-by: Colin Xu <colin.xu@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20201027045406.159566-1-colin.xu@intel.com
    Signed-off-by: Yongxin Liu <yongxin.liu@windriver.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1266,6 +1266,8 @@ static int i915_drm_resume(struct drm_device *dev)
 
 	intel_power_domains_enable(dev_priv);
 
+	intel_gvt_resume(dev_priv);
+
 	enable_rpm_wakeref_asserts(&dev_priv->runtime_pm);
 
 	return 0;
