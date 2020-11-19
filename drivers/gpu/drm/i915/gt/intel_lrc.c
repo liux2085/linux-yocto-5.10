b03ea084f59192555ac4c6624ff925d5968babcd
Chris Wilson <chris@chris-wilson.co.uk>
2020-11-19 16:56:13 +0000

    drm/i915/gt: Update request status flags for debug pretty-printer
    commit 562675d09a351a81e0dfc9a9d7df0f5f4f2fb6a9 upstream.
    
    We plan to expand upon the number of available statuses for when we
    pretty-print the requests along the timelines, and so need a new set of
    flags. We have settled upon:
    
    	Unready [U]
    	  - initial status after being submitted, the request is not
    	    ready for execution as it is waiting for external fences
    
    	Ready [R]
    	  - all fences the request was waiting on have been signaled,
                and the request is now ready for execution and will be
    	    in a backend queue
    
    	  - a ready request may still need to wait on semaphores
    	    [internal fences]
    
    	Ready/virtual [V]
    	  - same as ready, but queued over multiple backends
    
    	Executing [E]
    	  - the request has been transferred from the backend queue and
    	    submitted for execution on HW
    
    	  - a completed request may still be regarded as executing, its
    	    status may not be updated until it is retired and removed
    	    from the lists
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201119165616.10834-3-chris@chris-wilson.co.uk
    Signed-off-by: Yongxin Liu <yongxin.liu@windriver.com>

--- drivers/gpu/drm/i915/gt/intel_lrc.c
+++ drivers/gpu/drm/i915/gt/intel_lrc.c
@@ -6002,7 +6002,8 @@ void intel_execlists_show_requests(struct intel_engine_cs *engine,
 				   struct drm_printer *m,
 				   void (*show_request)(struct drm_printer *m,
 							const struct i915_request *rq,
-							const char *prefix),
+							const char *prefix,
+							int indent),
 				   unsigned int max)
 {
 	const struct intel_engine_execlists *execlists = &engine->execlists;
@@ -6017,7 +6018,7 @@ void intel_execlists_show_requests(struct intel_engine_cs *engine,
 	count = 0;
 	list_for_each_entry(rq, &engine->active.requests, sched.link) {
 		if (count++ < max - 1)
-			show_request(m, rq, "\t\tE ");
+			show_request(m, rq, "\t\t", 0);
 		else
 			last = rq;
 	}
@@ -6027,7 +6028,7 @@ void intel_execlists_show_requests(struct intel_engine_cs *engine,
 				   "\t\t...skipping %d executing requests...\n",
 				   count - max);
 		}
-		show_request(m, last, "\t\tE ");
+		show_request(m, last, "\t\t", 0);
 	}
 
 	if (execlists->switch_priority_hint != INT_MIN)
@@ -6045,7 +6046,7 @@ void intel_execlists_show_requests(struct intel_engine_cs *engine,
 
 		priolist_for_each_request(rq, p, i) {
 			if (count++ < max - 1)
-				show_request(m, rq, "\t\tQ ");
+				show_request(m, rq, "\t\t", 0);
 			else
 				last = rq;
 		}
@@ -6056,7 +6057,7 @@ void intel_execlists_show_requests(struct intel_engine_cs *engine,
 				   "\t\t...skipping %d queued requests...\n",
 				   count - max);
 		}
-		show_request(m, last, "\t\tQ ");
+		show_request(m, last, "\t\t", 0);
 	}
 
 	last = NULL;
@@ -6068,7 +6069,7 @@ void intel_execlists_show_requests(struct intel_engine_cs *engine,
 
 		if (rq) {
 			if (count++ < max - 1)
-				show_request(m, rq, "\t\tV ");
+				show_request(m, rq, "\t\t", 0);
 			else
 				last = rq;
 		}
@@ -6079,7 +6080,7 @@ void intel_execlists_show_requests(struct intel_engine_cs *engine,
 				   "\t\t...skipping %d virtual requests...\n",
 				   count - max);
 		}
-		show_request(m, last, "\t\tV ");
+		show_request(m, last, "\t\t", 0);
 	}
 
 	spin_unlock_irqrestore(&engine->active.lock, flags);
